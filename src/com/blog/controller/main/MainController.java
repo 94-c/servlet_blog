accd4b5635593170823ffe0f49a75d71416901ab
94-c <hyeongwoo26@gmail.com>
2022-06-09 22:39:14 +0900

    BoardDAO 작성

--- src/com/blog/controller/main/MainController.java
+++ src/com/blog/controller/main/MainController.java
@@ -9,6 +9,7 @@ public class MainController implements Action {
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
         System.out.println("main 경로 진입");
+        System.out.println("main 경로 진입 2");
         return "RequestDispatcher:/jsp/main/main.jsp";
     }
 }
